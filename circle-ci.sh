6debf5f7adc99d8d53ec9758fda737188ed9029e
Jerry Fernandes <jerry@paperkite.co.nz>
2015-10-28 16:11:07 +1300

    Shell script to override emulator dialog

---
+++ circle-ci.sh
@@ -0,0 +1,11 @@
+function pushHacks(){
+   HACKED="$ANDROID_HOME/hacks-applied"
+
+   if [ ! -e $HACKED ]; then
+       adb install -r circleci/android_emulator_hacks.apk &&
+       adb shell pm grant no.finn.android_emulator_hacks android.permission.SET_ANIMATION_SCALE &&
+       touch $HACKED
+   fi
+
+   adb shell am start -n no.finn.android_emulator_hacks/no.finn.android_emulator_hacks.HackActivity
+}
